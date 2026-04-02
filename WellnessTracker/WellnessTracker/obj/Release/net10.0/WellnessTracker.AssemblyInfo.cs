8cdab791cb45173d4aee5e8ffe629cdaab0f6f30
Daniel Jones <DanielJones.Code@yahoo.com>
2026-04-02 14:59:04 -0400

    Release Build
    2.0 release has been built.

--- WellnessTracker/WellnessTracker/obj/Release/net10.0/WellnessTracker.AssemblyInfo.cs
+++ WellnessTracker/WellnessTracker/obj/Release/net10.0/WellnessTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WellnessTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da50e0e55b2fd86f2e1d6ddc7b4cc681a9edaa44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d43bdeae7f2e730b932a3cf686cdee1fad2d2e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WellnessTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WellnessTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
